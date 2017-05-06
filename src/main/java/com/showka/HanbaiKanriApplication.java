a2c1aa5ae9f6dd21e1c50ee364381f97f16c9712
John Doe <johndoe@example.com>
2017-05-06 18:52:24 +0900

    make project

---
+++ src/main/java/com/showka/HanbaiKanriApplication.java
@@ -0,0 +1,12 @@
+package com.showka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbaiKanriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbaiKanriApplication.class, args);
+	}
+}
