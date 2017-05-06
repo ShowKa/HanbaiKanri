a3c7dcf682c0045a4b820ab9289d159bd745af19
John Doe <johndoe@example.com>
2017-05-07 00:55:25 +0900

    First Controller

---
+++ src/main/java/com/showka/u01/web/U01G001Controller.java
@@ -0,0 +1,20 @@
+package com.showka.u01.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class U01G001Controller {
+
+	// public method called by request
+	@RequestMapping(value = "/U01G001", method = RequestMethod.GET)
+	public String index(Map<String, Object> model, HttpSession session) {
+		return "/u01/u01g001";
+	}
+
+}
