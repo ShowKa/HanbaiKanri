1cd8b74946a9bcaf0e2c632279fdee8cdfc348a3
John Doe <johndoe@example.com>
2017-05-11 20:08:45 +0900

    Menu 画面

---
+++ src/main/java/com/showka/u00/web/U00G001Controller.java
@@ -0,0 +1,26 @@
+package com.showka.u00.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Menu.
+ * 
+ * @author Administrator
+ *
+ */
+@Controller
+public class U00G001Controller {
+
+	// public method called by request
+	@RequestMapping(value = "/U00G001", method = RequestMethod.GET)
+	public String index(Map<String, Object> model, HttpSession session) {
+		return "/u00/u00g001";
+	}
+
+}
