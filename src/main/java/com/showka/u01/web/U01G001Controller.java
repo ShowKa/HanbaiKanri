411d11bdf41a65f075f25463c8c5d7c2a3de1b07
John Doe <johndoe@example.com>
2017-07-02 01:44:48 +0900

    顧客検索

--- src/main/java/com/showka/u01/web/U01G001Controller.java
+++ src/main/java/com/showka/u01/web/U01G001Controller.java
@@ -1,15 +1,22 @@
 package com.showka.u01.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.showka.entity.MKokyaku;
+import com.showka.u01.service.i.MKokyakuCrudService;
 
 /**
  * U01G001 顧客検索画面
@@ -20,6 +27,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @EnableAutoConfiguration
 public class U01G001Controller {
+
+	@Autowired
+	private MKokyakuCrudService service;
 
 	// public method called by request
 	/**
@@ -42,7 +52,21 @@ public class U01G001Controller {
 	 */
 	@RequestMapping(value = "/u01g001/search", method = RequestMethod.GET)
 	public String search(@ModelAttribute U01G001Form form, Model model, HttpSession session) {
-		model.addAttribute("kokyakuName", form.getKokyakuName());
+
+		// 検索
+		List<MKokyaku> result = service.getKokyauByName(form.getKokyakuName());
+
+		// list
+		ArrayList<HashMap<String, String>> kokyakuList = new ArrayList<>();
+		for (MKokyaku k : result) {
+			HashMap<String, String> m = new HashMap<String, String>();
+			m.put("name", k.getName());
+			m.put("bushoName", k.getShukanBusho().getName());
+			kokyakuList.add(m);
+		}
+		model.addAttribute("kokyakuList", kokyakuList);
+
+		// 画面
 		return "/u01/u01g001KokyakuList :: list";
 	}
 
