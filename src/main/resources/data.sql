ca6dc5b1c195781621fc80fd80171186a069f08a
John Doe <johndoe@example.com>
2017-07-23 16:39:46 +0900

    bug fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,10 +3,9 @@ INSERT INTO m_busho
   ('BS01', '00', '00', '部署01', 'BS01'),
   ('BS02', '00', '00', '部署02', 'BS02');
 
-
 INSERT INTO m_kokyaku
 ( code, name, address, gentei_kubun, kokyaku_kubun,
-			shukan_busho_id, record_id )
+			shukan_busho_id, record_id ) VALUES
 ('KK01', 'aaaa', '左京区', '00', '01', 'BS01', 'KK01'),
 ('KK02', 'aaaa', '右京区', '00', '01', 'BS02', 'KK02'),
 ('KK03', 'bbbb', '上京区', '00', '01', 'BS02', 'KK03');
