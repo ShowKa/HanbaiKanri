fba0be602bf3daa048cbfcd643d936d0f36ae1b5
John Doe <johndoe@example.com>
2017-09-06 00:42:22 +0900

    imple NyukinKakeInfoCrudService

--- src/test/java/com/showka/service/crud/u01/NyukinKakeInfoCrudServiceImplTest.java
+++ src/test/java/com/showka/service/crud/u01/NyukinKakeInfoCrudServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.showka.service.crud.u01;
 
+import javax.transaction.Transactional;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
@@ -19,6 +22,30 @@ public class NyukinKakeInfoCrudServiceImplTest extends ServiceCrudTestCase {
 
 	@Autowired
 	private MNyukinKakeInfoRepository repo;
+
+	/**
+	 * table name
+	 */
+	private static final String TABLE_NAME = "m_nyukin_kake_info";
+
+	/**
+	 * columns
+	 */
+	private static final String[] COLUMN = { "kokyaku_id", "nyukin_hoho_kubun", "nyukin_tsuki_kubun", "shimebi",
+			"nyukin_date", "record_id" };
+
+	/**
+	 * test data
+	 */
+	private static final Object[] VALUE01 = { "KK03", "00", "10", "20", "30", "KK03" };
+
+	/**
+	 * Before
+	 */
+	@Before
+	public void before() {
+		super.deleteAll(TABLE_NAME);
+	}
 
 	/**
 	 * 新しいレコードのINERTテスト
@@ -46,7 +73,7 @@ public class NyukinKakeInfoCrudServiceImplTest extends ServiceCrudTestCase {
 		// save = insert
 		service.save(domain);
 
-		// assert
+		// check
 		MNyukinKakeInfo actual = repo.findById(id).get();
 		assertEquals(record_id, actual.getRecordId());
 
@@ -58,8 +85,11 @@ public class NyukinKakeInfoCrudServiceImplTest extends ServiceCrudTestCase {
 	@Test
 	public void test_update() {
 
+		// insert data
+		super.insert(TABLE_NAME, COLUMN, VALUE01);
+
 		String id = "KK03";
-		Integer version = 1;
+		Integer version = 0;
 		String record_id = "this is updated record";
 
 		// set up builder
@@ -78,7 +108,7 @@ public class NyukinKakeInfoCrudServiceImplTest extends ServiceCrudTestCase {
 		// save = update
 		service.save(domain);
 
-		// assert
+		// check
 		MNyukinKakeInfo actual = repo.findById(id).get();
 		assertEquals(record_id, actual.getRecordId());
 
@@ -93,6 +123,9 @@ public class NyukinKakeInfoCrudServiceImplTest extends ServiceCrudTestCase {
 	 */
 	@Test(expected = ObjectOptimisticLockingFailureException.class)
 	public void test_optimistic_lock_error() {
+
+		// insert data
+		super.insert(TABLE_NAME, COLUMN, VALUE01);
 
 		String id = "KK03";
 		Integer version = 999;
@@ -114,5 +147,75 @@ public class NyukinKakeInfoCrudServiceImplTest extends ServiceCrudTestCase {
 		// save = update
 		service.save(domain);
 		fail();
+	}
+
+	@Test
+	@Transactional
+	public void test_deleted() {
+
+		// insert data
+		super.insert(TABLE_NAME, COLUMN, VALUE01);
+
+		String id = "KK03";
+		Integer version = 0;
+
+		// delete
+		assertTrue(repo.existsById(id));
+		service.delete(id, version);
+
+		// check
+		assertFalse(repo.existsById(id));
+	}
+
+	@Test
+	@Transactional
+	public void test_getDomain() {
+
+		// insert data
+		super.insert(TABLE_NAME, COLUMN, VALUE01);
+
+		String id = "KK03";
+
+		// do
+		NyukinKakeInfoDomain actual = service.getDomain(id);
+
+		// check
+		assertEquals("KK03", actual.getKokyakuId());
+		assertEquals("KK03", actual.getRecordId());
+		assertEquals(Integer.valueOf(30), actual.getNyukinDate());
+		assertEquals("00", actual.getNyukinHohoKubun().getCode());
+		assertEquals("10", actual.getNyukinTsukiKubun().getCode());
+		assertEquals(Integer.valueOf(20), actual.getShimeDate());
+
+	}
+
+	@Test
+	public void test_exists01() {
+
+		// insert data
+		super.insert(TABLE_NAME, COLUMN, VALUE01);
+
+		String id = "KK03";
+
+		// do
+		boolean actual = service.exsists(id);
+
+		// check
+		assertTrue(actual);
+	}
+
+	@Test
+	public void test_exists02() {
+
+		// insert data
+		super.insert(TABLE_NAME, COLUMN, VALUE01);
+
+		String id = "AAAAAAA";
+
+		// do
+		boolean actual = service.exsists(id);
+
+		// check
+		assertFalse(actual);
 	}
 }
