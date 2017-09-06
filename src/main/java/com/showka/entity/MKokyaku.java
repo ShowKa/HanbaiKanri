e296b2597d9e5842dae2301b8310b3bb5fb3cd71
John Doe <JohnDoe@sample.com>
2017-09-06 12:59:36 +0900

    edit javadoc

--- src/main/java/com/showka/entity/MKokyaku.java
+++ src/main/java/com/showka/entity/MKokyaku.java
@@ -25,25 +25,32 @@ public class MKokyaku extends EntityBase implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	/** 顧客コード */
 	@Id
 	@Column(name = "code", unique = true, nullable = false, length = 4)
 	private String code;
 
+	/** 顧客名 */
 	@Column(name = "name", nullable = false, length = 255)
 	private String name;
 
+	/** 顧客住所 */
 	@Column(name = "address", nullable = false, length = 255)
 	private String address;
 
+	/** 顧客区分 */
 	@Column(name = "kokyaku_kubun", nullable = false, length = 2)
 	private String kokyakuKubun;
 
+	/** 販売区分 */
 	@Column(name = "hanbai_kubun", nullable = false)
 	private String hanbaiKubun;
 
+	/** 主幹部署ID */
 	@Column(name = "shukan_busho_id", nullable = false, length = 255)
 	private String shukanBushoId;
 
+	/** 主幹部署 */
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "shukan_busho_id", referencedColumnName = "record_id", insertable = false, updatable = false)
 	private MBusho shukanBusho;
