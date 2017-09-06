0e89a7eab72abc969633f20c090190dc9ee3cac5
John Doe <JohnDoe@sample.com>
2017-09-06 15:58:38 +0900

    correct BushoSrudServiceImlp & create new BushoCrudServiceTest

--- src/main/java/com/showka/service/crud/z00/BushoCrudServiceImpl.java
+++ src/main/java/com/showka/service/crud/z00/BushoCrudServiceImpl.java
@@ -1,15 +1,18 @@
 package com.showka.service.crud.z00;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.showka.domain.BushoDomain;
 import com.showka.domain.builder.BushoDomainBuilder;
 import com.showka.entity.MBusho;
 import com.showka.kubun.BushoKubun;
 import com.showka.kubun.JigyoKubun;
+import com.showka.kubun.i.Kubun;
 import com.showka.repository.i.MBushoRepository;
 import com.showka.service.crud.z00.i.BushoCrudService;
 
+@Service
 public class BushoCrudServiceImpl implements BushoCrudService {
 
 	@Autowired
@@ -23,8 +26,8 @@ public class BushoCrudServiceImpl implements BushoCrudService {
 		BushoDomainBuilder builder = new BushoDomainBuilder();
 		builder.withCode(result.getCode());
 		builder.withName(result.getName());
-		builder.withBushoKubun(BushoKubun.valueOf(result.getBushoKubun()));
-		builder.withJigyoKubun(JigyoKubun.valueOf(result.getJigyoKubun()));
+		builder.withBushoKubun(Kubun.get(BushoKubun.class, result.getBushoKubun()));
+		builder.withJigyoKubun(Kubun.get(JigyoKubun.class, result.getJigyoKubun()));
 		builder.withRecordId(result.getRecordId());
 		builder.withVersion(result.getVersion());
 
