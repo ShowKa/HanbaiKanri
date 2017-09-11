5fa08e9d8aea1335dc2b4c5661a1ba0b21d6ade9
John Doe <JohnDoe@sample.com>
2017-09-11 18:14:31 +0900

    along with Domein design changes

--- src/main/java/com/showka/service/crud/u01/KokyakuCrudServiceImpl.java
+++ src/main/java/com/showka/service/crud/u01/KokyakuCrudServiceImpl.java
@@ -4,8 +4,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.showka.domain.BushoDomain;
 import com.showka.domain.KokyakuDomain;
+import com.showka.domain.NyukinKakeInfoDomain;
 import com.showka.domain.builder.KokyakuDomainBuilder;
 import com.showka.entity.MKokyaku;
 import com.showka.kubun.HanbaiKubun;
@@ -13,6 +16,8 @@ import com.showka.kubun.KokyakuKubun;
 import com.showka.kubun.i.Kubun;
 import com.showka.repository.i.MKokyakuRepository;
 import com.showka.service.crud.u01.i.KokyakuCrudService;
+import com.showka.service.crud.u01.i.NyukinKakeInfoCrudService;
+import com.showka.service.crud.z00.i.BushoCrudService;
 
 /**
  * 顧客 CRUD Service
@@ -25,6 +30,12 @@ public class KokyakuCrudServiceImpl implements KokyakuCrudService {
 
 	@Autowired
 	private MKokyakuRepository repo;
+
+	@Autowired
+	private NyukinKakeInfoCrudService nyukinCrudService;
+
+	@Autowired
+	private BushoCrudService bushoService;
 
 	@Override
 	public void save(KokyakuDomain domain) {
@@ -39,38 +50,63 @@ public class KokyakuCrudServiceImpl implements KokyakuCrudService {
 		e.setAddress(domain.getAddress());
 		e.setKokyakuKubun(domain.getKokyakuKubun().getCode());
 		e.setHanbaiKubun(domain.getHanbaiKubun().getCode());
-		e.setShukanBushoId(domain.getShukanBushoId());
+		e.setShukanBushoId(domain.getShukanBusho().getRecordId());
 
 		// set record_id & version
 		e.setRecordId(domain.getRecordId());
 		e.setVersion(domain.getVersion());
 
-		// save
+		// 顧客save
 		repo.save(e);
+
+		// 入金掛情報save
+		NyukinKakeInfoDomain nyukinKakeInfo = domain.getNyukinKakeInfo();
+		if (domain.getHanbaiKubun() == HanbaiKubun.掛売) {
+			nyukinCrudService.save(nyukinKakeInfo);
+		} else {
+			if (nyukinCrudService.exsists(domain.getCode())) {
+				NyukinKakeInfoDomain target = nyukinCrudService.getDomain(domain.getCode());
+				nyukinCrudService.delete(target.getKokyakuId(), target.getVersion());
+			}
+		}
 	}
 
 	@Override
+	@Transactional
 	public void delete(String code, Integer version) {
-		MKokyaku target = new MKokyaku();
-		target.setCode(code);
-		target.setVersion(version);
-		repo.delete(target);
+		MKokyaku targetKokyaku = new MKokyaku();
+		targetKokyaku.setCode(code);
+		targetKokyaku.setVersion(version);
+
+		// 入金掛情報があれば削除
+		if (nyukinCrudService.exsists(code)) {
+			Integer nyukinKakeInfoVersion = nyukinCrudService.getDomain(code).getVersion();
+			nyukinCrudService.delete(code, nyukinKakeInfoVersion);
+		}
+
+		// 顧客を削除
+		repo.delete(targetKokyaku);
 	}
 
 	@Override
 	public KokyakuDomain getDomain(String pk) {
-		MKokyaku result = repo.getOne(pk);
+		MKokyaku kokyakuEntity = repo.getOne(pk);
 
 		KokyakuDomainBuilder builder = new KokyakuDomainBuilder();
-		builder.withCode(result.getCode());
-		builder.withAddress(result.getAddress());
-		builder.withName(result.getName());
-		builder.withKokyakuKubun(Kubun.get(KokyakuKubun.class, result.getKokyakuKubun()));
-		builder.withHanbaiKubun(Kubun.get(HanbaiKubun.class, result.getHanbaiKubun()));
-		builder.withShukanBushoId(result.getShukanBushoId());
+		builder.withCode(kokyakuEntity.getCode());
+		builder.withAddress(kokyakuEntity.getAddress());
+		builder.withName(kokyakuEntity.getName());
+		builder.withKokyakuKubun(Kubun.get(KokyakuKubun.class, kokyakuEntity.getKokyakuKubun()));
+		builder.withHanbaiKubun(Kubun.get(HanbaiKubun.class, kokyakuEntity.getHanbaiKubun()));
 
-		builder.withRecordId(result.getRecordId());
-		builder.withVersion(result.getVersion());
+		BushoDomain buhoDomain = bushoService.getDomain(kokyakuEntity.getShukanBusho().getRecordId());
+		builder.withShukanBusho(buhoDomain);
+
+		NyukinKakeInfoDomain nyukinKakeInfoDomain = nyukinCrudService.getDomain(kokyakuEntity.getCode());
+		builder.withNyukinKakeInfo(nyukinKakeInfoDomain);
+
+		builder.withRecordId(kokyakuEntity.getRecordId());
+		builder.withVersion(kokyakuEntity.getVersion());
 
 		return builder.build();
 	}
--- src/test/java/com/showka/service/crud/u01/KokyakuCrudServiceImplTest.java
+++ src/test/java/com/showka/service/crud/u01/KokyakuCrudServiceImplTest.java
@@ -5,16 +5,24 @@ import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.showka.common.ServiceCrudTestCase;
+import com.showka.domain.BushoDomain;
 import com.showka.domain.KokyakuDomain;
+import com.showka.domain.NyukinKakeInfoDomain;
 import com.showka.domain.builder.KokyakuDomainBuilder;
+import com.showka.domain.builder.NyukinKakeInfoDomainBuilder;
 import com.showka.entity.MKokyaku;
+import com.showka.entity.MNyukinKakeInfo;
 import com.showka.kubun.HanbaiKubun;
 import com.showka.kubun.KokyakuKubun;
+import com.showka.kubun.NyukinHohoKubun;
+import com.showka.kubun.NyukinTsukiKubun;
 import com.showka.repository.i.MKokyakuRepository;
+import com.showka.repository.i.MNyukinKakeInfoRepository;
+import com.showka.service.crud.u01.i.NyukinKakeInfoCrudService;
+import com.showka.service.crud.z00.i.BushoCrudService;
 
 /**
  * 顧客 CRUD Service Test
@@ -29,6 +37,15 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 
 	@Autowired
 	private MKokyakuRepository repo;
+
+	@Autowired
+	private MNyukinKakeInfoRepository nyukinRepo;
+
+	@Autowired
+	private BushoCrudService bushoService;
+
+	@Autowired
+	private NyukinKakeInfoCrudService nyukinCrudService;
 
 	/**
 	 * table name
@@ -48,40 +65,96 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 	private static final Object[] VALUE02 = { "KK02", "aaaa", "右京区", "00", "01", "BS02", "KK02" };
 	private static final Object[] VALUE03 = { "KK03", "bbbb", "上京区", "10", "01", "BS02", "KK03" };
 
+	/**
+	 * table name
+	 */
+	private static final String BUSHO_TABLE_NAME = "m_busho";
+
+	/**
+	 * columns
+	 */
+	private static final String[] BUSHO_COLUMN = { "code", "busho_kubun", "jigyo_kubun", "name", "record_id" };
+
+	/**
+	 * test data
+	 */
+	private static final Object[] BUSHO_VALUE01 = { "BS01", "01", "01", "部署01", "BS01" };
+	private static final Object[] BUSHO_VALUE02 = { "BS02", "99", "02", "部署02", "BS02" };
+
+	/**
+	 * table name
+	 */
+	private static final String NYUKIN_KAKE_TABLE_NAME = "m_nyukin_kake_info";
+
+	/**
+	 * columns
+	 */
+	private static final String[] NYUKIN_KAKE_COLUMN = { "kokyaku_id", "nyukin_hoho_kubun", "nyukin_tsuki_kubun",
+			"shimebi", "nyukin_date", "record_id" };
+
+	/**
+	 * test data
+	 */
+	private static final Object[] NYUKIN_KAKE_VALUE01 = { "KK03", "00", "10", "20", "30", "KK03" };
+
 	@Before
 	public void deletTable() {
 		super.deleteAll(TABLE_NAME);
+		super.deleteAll(BUSHO_TABLE_NAME);
+		super.deleteAll(NYUKIN_KAKE_TABLE_NAME);
+
+		super.insert(BUSHO_TABLE_NAME, BUSHO_COLUMN, BUSHO_VALUE01, BUSHO_VALUE02);
 	}
 
 	/**
 	 * 新しいレコードのINERTテスト
 	 */
 	@Test
+	@Transactional
 	public void test_insert() {
 
+		// kokyaku
 		final String id = "NEW!";
 		final String name = "aaaa";
 		final String address = "北区";
-		final KokyakuKubun kKubun = KokyakuKubun.法人;
-		final HanbaiKubun hKubun = HanbaiKubun.掛売;
+		final KokyakuKubun kokyakuKubun = KokyakuKubun.法人;
+		final HanbaiKubun hanbaiKubun = HanbaiKubun.掛売;
 		final String bushoId = "BS01";
 
 		final Integer version = 0;
 		final String record_id = "this is inserted record";
 
-		// set up builder
+		// nyukinKakeInfo
+		final Integer nyukinDate = 11;
+		final NyukinHohoKubun nyukinHohoKubun = NyukinHohoKubun.振込;
+		final NyukinTsukiKubun nyukinTukiKubun = NyukinTsukiKubun.当月;
+		final Integer shimeDate = 12;
+
+		// build nyukinKakeInfo domain
+		NyukinKakeInfoDomainBuilder nyukinBuilder = new NyukinKakeInfoDomainBuilder();
+		nyukinBuilder.withKokyakuId(id);
+		nyukinBuilder.withNyukinDate(nyukinDate);
+		nyukinBuilder.withNyukinHohoKubun(nyukinHohoKubun);
+		nyukinBuilder.withNyukinTsukiKubun(nyukinTukiKubun);
+		nyukinBuilder.withShimeDate(shimeDate);
+		nyukinBuilder.withVersion(version);
+		nyukinBuilder.withRecordId(record_id);
+		NyukinKakeInfoDomain nyukinDomain = nyukinBuilder.build();
+
+		// get busho domain
+		BushoDomain bushoDomain = bushoService.getDomain(bushoId);
+
+		// build kokyaku domain
 		KokyakuDomainBuilder builder = new KokyakuDomainBuilder();
 		builder.withCode(id);
 		builder.withName(name);
 		builder.withAddress(address);
-		builder.withKokyakuKubun(kKubun);
-		builder.withHanbaiKubun(hKubun);
-		builder.withShukanBushoId(bushoId);
-
+		builder.withKokyakuKubun(kokyakuKubun);
+		builder.withHanbaiKubun(hanbaiKubun);
+		builder.withShukanBusho(bushoDomain);
+		builder.withNyukinKakeInfo(nyukinDomain);
 		builder.withVersion(version);
 		builder.withRecordId(record_id);
-
-		// build domain
 		KokyakuDomain domain = builder.build();
 
 		// save = insert
@@ -91,12 +164,15 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 		MKokyaku actual = repo.findById(id).get();
 		assertEquals(name, actual.getName());
 		assertEquals(address, actual.getAddress());
-		assertEquals(kKubun.getCode(), actual.getKokyakuKubun());
-		assertEquals(hKubun.getCode(), actual.getHanbaiKubun());
-		assertEquals(name, actual.getName());
-		assertEquals(bushoId, actual.getShukanBushoId());
-
+		assertEquals(kokyakuKubun.getCode(), actual.getKokyakuKubun());
+		assertEquals(hanbaiKubun.getCode(), actual.getHanbaiKubun());
 		assertEquals(record_id, actual.getRecordId());
+
+		MNyukinKakeInfo actualNyukin = nyukinRepo.findById(id).get();
+		assertEquals(nyukinHohoKubun.getCode(), actualNyukin.getNyukinHohoKubun());
+		assertEquals(nyukinTukiKubun.getCode(), actualNyukin.getNyukinTsukiKubun());
+		assertEquals(shimeDate.intValue(), actualNyukin.getShimebi());
+		assertEquals(nyukinDate.intValue(), actualNyukin.getNyukinDate());
 
 	}
 
@@ -104,84 +180,56 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 	 * 既存レコードの更新テスト
 	 */
 	@Test
+	@Transactional
 	public void test_update() {
 
 		super.insert(TABLE_NAME, COLUMN, VALUE01, VALUE02, VALUE03);
+		super.insert(NYUKIN_KAKE_TABLE_NAME, NYUKIN_KAKE_COLUMN, NYUKIN_KAKE_VALUE01);
 
-		final String id = "KK01";
-		final String name = "abcd";
-		final String address = "北区";
-		final KokyakuKubun kKubun = KokyakuKubun.法人;
-		final HanbaiKubun hKubun = HanbaiKubun.掛売;
+		// kokyaku
+		final String id = "KK03";
+		final String name = "ccbb";
+		final String address = "南区";
+		final KokyakuKubun kokyakuKubun = KokyakuKubun.法人;
+		final HanbaiKubun hanbaiKubun = HanbaiKubun.現金;
 		final String bushoId = "BS01";
 
 		final Integer version = 0;
-		final String record_id = "this is updated record";
+		final String record_id = "this is inserted record";
 
-		// set up builder
+		// build nyukinKakeInfo domain
+		NyukinKakeInfoDomainBuilder nyukinBuilder = new NyukinKakeInfoDomainBuilder();
+		NyukinKakeInfoDomain nyukinDomain = nyukinBuilder.build();
+
+		// get busho domain
+		BushoDomain bushoDomain = bushoService.getDomain(bushoId);
+
+		// build kokyaku domain
 		KokyakuDomainBuilder builder = new KokyakuDomainBuilder();
 		builder.withCode(id);
 		builder.withName(name);
 		builder.withAddress(address);
-		builder.withKokyakuKubun(kKubun);
-		builder.withHanbaiKubun(hKubun);
-		builder.withShukanBushoId(bushoId);
-
+		builder.withKokyakuKubun(kokyakuKubun);
+		builder.withHanbaiKubun(hanbaiKubun);
+		builder.withShukanBusho(bushoDomain);
+		builder.withNyukinKakeInfo(nyukinDomain);
 		builder.withVersion(version);
 		builder.withRecordId(record_id);
-
-		// build domain
 		KokyakuDomain domain = builder.build();
 
-		// save = update
+		// save = insert
 		service.save(domain);
 
 		// assert
 		MKokyaku actual = repo.findById(id).get();
 		assertEquals(name, actual.getName());
 		assertEquals(address, actual.getAddress());
-		assertEquals(kKubun.getCode(), actual.getKokyakuKubun());
-		assertEquals(hKubun.getCode(), actual.getHanbaiKubun());
-		assertEquals(name, actual.getName());
-		assertEquals(bushoId, actual.getShukanBushoId());
-
+		assertEquals(kokyakuKubun.getCode(), actual.getKokyakuKubun());
+		assertEquals(hanbaiKubun.getCode(), actual.getHanbaiKubun());
 		assertEquals(record_id, actual.getRecordId());
-	}
 
-	/**
-	 * 排他制御テスト
-	 *
-	 * <pre>
-	 * 不正なversion番号を設定する。更新エラーとなればよい。
-	 * </pre>
-	 */
-	@Test(expected = ObjectOptimisticLockingFailureException.class)
-	public void test_optimistic_lock_error() {
-
-		super.insert(TABLE_NAME, COLUMN, VALUE01, VALUE02, VALUE03);
-
-		String id = "KK01";
-		Integer version = 999;
-		String record_id = "this is inserted record";
-
-		// set up builder
-		KokyakuDomainBuilder builder = new KokyakuDomainBuilder();
-		builder.withCode(id);
-		builder.withName("aaaa");
-		builder.withAddress("北区");
-		builder.withKokyakuKubun(KokyakuKubun.法人);
-		builder.withHanbaiKubun(HanbaiKubun.掛売);
-		builder.withShukanBushoId("BS01");
-
-		builder.withVersion(version);
-		builder.withRecordId(record_id);
-
-		// build domain
-		KokyakuDomain domain = builder.build();
-
-		// save = update
-		service.save(domain);
-		fail();
+		Optional<MNyukinKakeInfo> actualNyukin = nyukinRepo.findById(id);
+		assertFalse(actualNyukin.isPresent());
 
 	}
 
@@ -193,6 +241,7 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 	public void test_delete1() {
 
 		super.insert(TABLE_NAME, COLUMN, VALUE01, VALUE02, VALUE03);
+		super.insert(NYUKIN_KAKE_TABLE_NAME, NYUKIN_KAKE_COLUMN, NYUKIN_KAKE_VALUE01);
 
 		String id = "KK03";
 		Integer version = 0;
@@ -203,7 +252,8 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 		// assert
 		Optional<MKokyaku> result = repo.findById(id);
 		assertFalse(result.isPresent());
-
+		Optional<MNyukinKakeInfo> resultNyukin = nyukinRepo.findById(id);
+		assertFalse(result.isPresent());
 	}
 
 	/**
@@ -241,6 +291,7 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 	public void test_getDomain1() {
 
 		super.insert(TABLE_NAME, COLUMN, VALUE01, VALUE02, VALUE03);
+		super.insert(NYUKIN_KAKE_TABLE_NAME, NYUKIN_KAKE_COLUMN, NYUKIN_KAKE_VALUE01);
 
 		String id = "KK03";
 
@@ -253,9 +304,13 @@ public class KokyakuCrudServiceImplTest extends ServiceCrudTestCase {
 		assertEquals("上京区", result.getAddress());
 		assertEquals("10", result.getHanbaiKubun().getCode());
 		assertEquals("01", result.getKokyakuKubun().getCode());
-		assertEquals("BS02", result.getShukanBushoId());
+		assertEquals("BS02", result.getShukanBusho().getRecordId());
 		assertEquals("KK03", result.getRecordId());
 
+		NyukinKakeInfoDomain resultNyukin = result.getNyukinKakeInfo();
+		assertEquals("00", resultNyukin.getNyukinHohoKubun().getCode());
+		assertEquals("10", resultNyukin.getNyukinTsukiKubun().getCode());
+		assertEquals(20, resultNyukin.getShimeDate().intValue());
+		assertEquals(30, resultNyukin.getNyukinDate().intValue());
 	}
-
 }
