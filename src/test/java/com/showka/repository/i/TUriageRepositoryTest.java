ff95162c75ecb3449e3e0bf76c812dcccdce6dd8
John Doe <johndoe@example.com>
2017-09-21 22:20:33 +0900

    RepositoryCustom : findByRecordId

--- src/test/java/com/showka/repository/i/TUriageRepositoryTest.java
+++ src/test/java/com/showka/repository/i/TUriageRepositoryTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.showka.common.RepositoryTestCase;
 import com.showka.entity.TUriage;
-import com.showka.repository.i.TUriageRepository;
 
 public class TUriageRepositoryTest extends RepositoryTestCase {
 
@@ -18,5 +17,11 @@ public class TUriageRepositoryTest extends RepositoryTestCase {
 	public void test_01() {
 		List<TUriage> actual = repository.findAll();
 		assertNotNull(actual.get(0));
+	}
+
+	@Test
+	public void test_02_recordId() {
+		TUriage actual = repository.findByRecordId("KK01-00001");
+		System.out.println(actual);
 	}
 }
