72ad4204ba6b9b7600095310867e10aa761e8f72
John Doe <JohnDoe@sample.com>
2017-09-25 15:58:03 +0900

    Implement updateButton, deleteButton

--- src/main/resources/static/u01/u01g002.js
+++ src/main/resources/static/u01/u01g002.js
@@ -1,3 +1,9 @@
 function register() {
 	crud("/u01g002/register", "registerForm", "/u01g002/refer");
 }
+function update() {
+	crud("/u01g002/update", "registerForm", "/u01g002/refer");
+}
+function deleteKokyaku() {
+	crud("/u01g002/delete", "registerForm", "/u01g001/");
+}
