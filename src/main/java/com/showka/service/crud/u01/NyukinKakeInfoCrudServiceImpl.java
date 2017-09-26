33c81d379f168d10bcea7a4e91e4eeeb22c84b34
John Doe <JohnDoe@sample.com>
2017-09-26 16:27:40 +0900

    add @Transactional

--- src/main/java/com/showka/service/crud/u01/NyukinKakeInfoCrudServiceImpl.java
+++ src/main/java/com/showka/service/crud/u01/NyukinKakeInfoCrudServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.showka.domain.NyukinKakeInfoDomain;
 import com.showka.domain.builder.NyukinKakeInfoDomainBuilder;
@@ -57,6 +58,7 @@ public class NyukinKakeInfoCrudServiceImpl implements NyukinKakeInfoCrudService 
 	}
 
 	@Override
+	@Transactional
 	public NyukinKakeInfoDomain getDomain(String kokyakuId) {
 		// get entity
 		MNyukinKakeInfo e = repo.getOne(kokyakuId);
