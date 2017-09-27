1a6542fc66c9c2ca271d5fabb900a29ab9937ddf
John Doe <JohnDoe@sample.com>
2017-09-27 16:59:19 +0900

    add updateForm button, form-control

--- src/main/resources/static/u01/u01g002.js
+++ src/main/resources/static/u01/u01g002.js
@@ -13,3 +13,14 @@ function update() {
 function deleteKokyaku() {
 	crud("/u01g002/delete", "registerForm", "/u01g001/");
 }
+function updateForm() {
+   var form = document.forms["registerForm"];
+
+   form.method = "GET"; // method(GET or POST)を設定する
+   form.action = "/u01g002/updateForm"; // action(遷移先URL)を設定する
+   form.submit(); // submit する
+}
+
+$(document).ready(function() {
+	swithElementActivationByMode();
+});
