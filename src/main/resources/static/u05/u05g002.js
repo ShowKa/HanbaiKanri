b3a1f1d5e23e0016bc8e0f0959dd1b27b35dc827
John Doe <johndoe@example.com>
2017-12-18 02:34:19 +0900

    test

--- src/main/resources/static/u05/u05g002.js
+++ src/main/resources/static/u05/u05g002.js
@@ -1,48 +1,10 @@
 
-// services
-ngModules.service('meisai', [ '$rootScope', '$filter',
-// 明細
-function($scope, $filster) {
-	
-	this.convertToMeisai = function(meisai, edit) {
-		if (meisai.editing) {
-			return;
-		}
-		edit = edit != null ? edit :false;
-		meisai.editing = edit;
-		meisai.edit = function (e) {
-			e = e != null ? e : true;
-			this.editing = e;
-		};
-	};
-	
-	this.convertCollectionToMeisai = function(meisaiList, edit) {
-		for (var l of meisaiList) {
-			this.convertToMeisai(l, edit);
-		}
-	};
-	
-	this.checkMeisais = function (meisaiList) {
-		if (meisaiList.length === 0) {
-			showErroeMessage("明細を追加してください。");
-			return false;
-		}
-		for ( var l of meisaiList) {
-			if (l.editing == true) {
-				showErroeMessage("編集中の明細が残っています。");
-				return false;
-			}
-		}
-		return true;
-	};
-}]);
-	
 ngModules.service('denpyo', [ '$rootScope', '$filter', 'meisai',
 // モデルの操作
 function($scope, $filter, meisai) {
 	
-	this.checkMeisais = function(meisaiList) {
-		return meisai.checkMeisais(meisaiList);
+	this.checkMeisaiList = function(meisaiList) {
+		return meisai.checkMeisaiList(meisaiList);
 	}
 
 	this.createMeisai = function() {
@@ -62,13 +24,22 @@ function($scope, $filter, meisai) {
 	this.getSubtotal = function (meisai) {
 		return meisai.hanbaiNumber * meisai.hanbaiTanka;
 	};
+	
+	this.getTotalAmount = function (meisaiList) {
+		var total = 0;
+		for (var m of meisaiList) {
+			total += this.getSubtotal(m);
+		}
+		return total;
+	}
 
 } ])
 // main controller
 .controller('MainController', [ '$scope', '$http', 'denpyo', 'common',
 // HDRと明細
 function($scope, $http, denpyo, common) {
-		
+
+	// validate header
 	var validateHeader = function (callback) {
 		$http({
 			method : 'POST',
@@ -88,6 +59,7 @@ function($scope, $http, denpyo, common) {
 		});
 	}
 
+	// validate meisai
 	var validateMeisai = function (meisai, callback) {
 		var meisai = {
 			"meisai[0].shohinCode" : meisai.shohinCode,
@@ -116,7 +88,7 @@ function($scope, $http, denpyo, common) {
 	// 明細入力完了
 	$scope.done = function(meisai) {
 		validateMeisai(meisai, function () {
-			meisai.edit(false);
+			meisai.editDone();
 		});
 	};
 
@@ -124,7 +96,7 @@ function($scope, $http, denpyo, common) {
 	$scope.register = function() {
 
 		// check
-		if (!denpyo.checkMeisais($scope.meisaiList)) {
+		if (!denpyo.checkMeisaiList($scope.meisaiList)) {
 			return;
 		}
 
@@ -145,7 +117,7 @@ function($scope, $http, denpyo, common) {
 	$scope.update = function() {
 
 		// check
-		if (!denpyo.checkMeisais($scope.meisaiList)) {
+		if (!denpyo.checkMeisaiList($scope.meisaiList)) {
 			return;
 		}
 
@@ -177,7 +149,7 @@ function($scope, $http, denpyo, common) {
 
 	// 編集開始
 	$scope.edit = function(meisai) {
-		meisai.edit(true);
+		meisai.edit();
 	}
 
 	// 任意の明細行をリストモデルから取り除く
@@ -196,11 +168,7 @@ function($scope, $http, denpyo, common) {
 
 	// 明細小計の合計
 	$scope.getTotalAmount = function(meisaiList) {
-		var totalAmount = 0;
-		angular.forEach(meisaiList, function(meisai) {
-			totalAmount += $scope.getSubtotal(meisai);
-		});
-		return totalAmount;
+		return denpyo.getTotalAmount(meisaiList);
 	};
 
 	// 初期化
@@ -214,8 +182,8 @@ function($scope, $http, denpyo, common) {
 		if(!$scope.meisaiList){
 			$scope.meisaiList = [];
 		}
-		$scope.$watchCollection('meisaiList', function(newMeisais, oldMeisais) {
-			denpyo.convertCollectionToMeisai(newMeisais);
+		$scope.$watchCollection('meisaiList', function(newMeisaiList, oldMeisaiList) {
+			denpyo.convertCollectionToMeisai(newMeisaiList);
 		});
 	};
 	
