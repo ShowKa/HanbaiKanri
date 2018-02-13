348d21bafbb41b3f6aba828d3020aee7854b8d46
John Doe <johndoe@example.com>
2018-02-13 20:37:54 +0900

    fix

--- src/test/java/com/showka/service/search/u01/KokyakuSearchServiceImplTest.java
+++ src/test/java/com/showka/service/search/u01/KokyakuSearchServiceImplTest.java
@@ -18,28 +18,12 @@ public class KokyakuSearchServiceImplTest extends ServiceCrudTestCase {
 	private KokyakuSearchServiceImpl service;
 
 	/**
-	 * table name
-	 */
-	private static final String M_KOKYAKU = "m_kokyaku";
-	/**
-	 * table name
-	 */
-	private static final String M_BUSHO = "m_busho";
-	/**
-	 * columns
-	 */
-	private static final String[] K_COLUMN = { "code", "name", "address", "hanbai_kubun", "kokyaku_kubun",
-			"shukan_busho_id", "record_id" };
-
-	private static final String[] B_COLUMN = { "code", "busho_kubun", "jigyo_kubun", "name", "record_id" };
-
-	/**
 	 * test data
 	 */
 	// kokyaku
-	private static final Object[] K_VALUE01 = { "KK01", "aaaa", "左京区", "00", "01", "BS01", "KK01" };
-	private static final Object[] K_VALUE02 = { "KK02", "aaaa", "右京区", "00", "01", "BS02", "KK02" };
-	private static final Object[] K_VALUE03 = { "KK03", "bbbb", "上京区", "00", "01", "BS02", "KK03" };
+	private static final Object[] K_VALUE01 = { "KK01", "aaaa", "左京区", "01", "00", "BS01", "KK01" };
+	private static final Object[] K_VALUE02 = { "KK02", "aaaa", "右京区", "01", "00", "BS02", "KK02" };
+	private static final Object[] K_VALUE03 = { "KK03", "bbbb", "上京区", "01", "00", "BS02", "KK03" };
 	// busho
 	private static final Object[] B_VALUE01 = { "BS01", "01", "01", "部署01", "BS01" };
 	private static final Object[] B_VALUE02 = { "BS02", "99", "02", "部署02", "BS02" };
@@ -50,9 +34,9 @@ public class KokyakuSearchServiceImplTest extends ServiceCrudTestCase {
 	@Before
 	public void before() {
 		super.deleteAll(M_KOKYAKU);
-		super.insert(M_KOKYAKU, K_COLUMN, K_VALUE01, K_VALUE02, K_VALUE03);
+		super.insert(M_KOKYAKU, M_KOKYAKU_COLUMN, K_VALUE01, K_VALUE02, K_VALUE03);
 		super.deleteAll(M_BUSHO);
-		super.insert(M_BUSHO, B_COLUMN, B_VALUE01, B_VALUE02);
+		super.insert(M_BUSHO, M_BUSHO_COLUMN, B_VALUE01, B_VALUE02);
 	}
 
 	@Test
