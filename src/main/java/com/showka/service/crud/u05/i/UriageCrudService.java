1c34c569ed97bdc67bce1aa0003ec7109327b39f
John Doe <johndoe@example.com>
2018-02-19 21:37:55 +0900

    add getUriageListOfKokyaku

--- src/main/java/com/showka/service/crud/u05/i/UriageCrudService.java
+++ src/main/java/com/showka/service/crud/u05/i/UriageCrudService.java
@@ -1,5 +1,7 @@
 package com.showka.service.crud.u05.i;
 
+import java.util.List;
+
 import com.showka.domain.Uriage;
 import com.showka.entity.TUriagePK;
 import com.showka.service.crud.CrudService;
@@ -24,5 +26,14 @@ public interface UriageCrudService extends CrudService<Uriage, TUriagePK> {
 	 * @return 売上
 	 */
 	public Uriage getDomain(String kokyakuCode, String denpyoNumber);
+
+	/**
+	 * 顧客の売上リストを取得する.
+	 * 
+	 * @param kokyakuCode
+	 *            顧客コード
+	 * @return 売上リスト
+	 */
+	public List<Uriage> getUriageOfKokyaku(String kokyakuCode);
 
 }
