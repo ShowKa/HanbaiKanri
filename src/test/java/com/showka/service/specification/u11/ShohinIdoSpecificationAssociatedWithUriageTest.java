5a0ba6cdc252fb5c398cdd1235548249291ebcee
test@example.com <Administrator@ZK0CA0C9.uodomain.local>
2018-03-14 17:27:01 +0900

    fix test

--- src/test/java/com/showka/service/specification/u11/ShohinIdoSpecificationAssociatedWithUriageTest.java
+++ src/test/java/com/showka/service/specification/u11/ShohinIdoSpecificationAssociatedWithUriageTest.java
@@ -2,6 +2,7 @@ package com.showka.service.specification.u11;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,10 +17,12 @@ import com.showka.domain.ShohinIdoMeisai;
 import com.showka.domain.ShohinZaiko;
 import com.showka.domain.Uriage;
 import com.showka.domain.UriageMeisai;
+import com.showka.domain.builder.BushoBuilder;
+import com.showka.domain.builder.KokyakuBuilder;
+import com.showka.domain.builder.ShohinBuilder;
 import com.showka.domain.builder.ShohinIdoBuilder;
 import com.showka.domain.builder.UriageBuilder;
 import com.showka.domain.builder.UriageMeisaiBuilder;
-import com.showka.entity.TUriagePK;
 import com.showka.kubun.HanbaiKubun;
 import com.showka.kubun.ShohinIdoKubun;
 import com.showka.service.crud.u05.i.UriageCrudService;
@@ -33,7 +36,6 @@ import com.showka.value.TheDate;
 
 import mockit.Expectations;
 import mockit.Injectable;
-import mockit.Mocked;
 import mockit.Tested;
 import mockit.Verifications;
 
@@ -224,65 +226,191 @@ public class ShohinIdoSpecificationAssociatedWithUriageTest extends SimpleTestCa
 		assertEquals(ShohinIdoKubun.売上訂正, ido.getKubun());
 	}
 
+	/**
+	 * 仕様確認.
+	 *
+	 * <pre>
+	 * 入力：売上<br>
+	 * 条件：在庫10個に対し、商品売上10個 <br>
+	 * 結果：エラーが発生しない。
+	 * 
+	 * <pre>
+	 */
 	@Test
-	public void test03_ascertainSatisfaction() throws Exception {
+	public void test03_ascertainSatisfaction(@Injectable ShohinZaiko zaiko) throws Exception {
+		// input
+		// 商品
+		ShohinBuilder sb = new ShohinBuilder();
+		sb.withCode("SH01");
+		Shohin shohin = sb.build();
+		// 明細
+		ArrayList<UriageMeisai> meisai = new ArrayList<UriageMeisai>();
+		// 明細01
+		UriageMeisaiBuilder umb = new UriageMeisaiBuilder();
+		umb.withHanbaiNumber(10);
+		umb.withShohinDomain(shohin);
+		UriageMeisai uriageMeisai = umb.build();
+		meisai.add(uriageMeisai);
+		// 部署
+		BushoBuilder bb = new BushoBuilder();
+		bb.withCode("BS01");
+		EigyoDate eigyoDate = new EigyoDate(2017, 8, 20);
+		bb.withEigyoDate(eigyoDate);
+		Busho busho = bb.build();
+		// 顧客
+		KokyakuBuilder kb = new KokyakuBuilder();
+		kb.withCode("KK01");
+		kb.withShukanBusho(busho);
+		Kokyaku kokyaku = kb.build();
+		// 売上
+		UriageBuilder ub = new UriageBuilder();
+		ub.withKokyaku(kokyaku);
+		ub.withDenpyoNumber("00001");
+		ub.withUriageMeisai(meisai);
+		Uriage uriage = ub.build();
 		// expect
 		new Expectations() {
 			{
-				uriageCrudService.exsists((TUriagePK) any);
-				result = false;
+				zaiko.getNumber();
+				result = 10;
 			}
 		};
 		// do
-		shohinIdoSpecificationImpl.setUriage(uriage00);
+		shohinIdoSpecificationImpl.setUriage(uriage);
 		shohinIdoSpecificationImpl.ascertainSatisfaction();
 		// check
 		assertTrue(true);
-		// verify
-		new Verifications() {
-			{
-				uriageCrudService.exsists((TUriagePK) any);
-				times = 1;
-			}
-		};
 	}
 
-	@Test
-	public void test04_ascertainSatisfaction(@Mocked ShohinZaiko zaiko) throws Exception {
-		// expect
-		new Expectations() {
-			{
-				uriageCrudService.exsists((TUriagePK) any);
-				result = false;
-				zaiko.getNumber();
-				result = 6;
-			}
-		};
-		// do
-		shohinIdoSpecificationImpl.setUriage(uriage02);
-		shohinIdoSpecificationImpl.ascertainSatisfaction();
-		// verify
-		new Verifications() {
-			{
-				uriageCrudService.exsists((TUriagePK) any);
-				times = 1;
-			}
-		};
-	}
-
+	/**
+	 * 仕様確認.
+	 *
+	 * <pre>
+	 * 入力：売上<br>
+	 * 条件：在庫10個に対し、商品売上11個 <br>
+	 * 結果：マイナス在庫例外
+	 * 
+	 * <pre>
+	 */
 	@Test(expected = MinusZaikoException.class)
-	public void test05_ascertainSatisfaction(@Mocked ShohinZaiko zaiko) throws Exception {
+	public void test04_ascertainSatisfaction(@Injectable ShohinZaiko zaiko) throws Exception {
+		// input
+		// 商品
+		ShohinBuilder sb = new ShohinBuilder();
+		sb.withCode("SH01");
+		Shohin shohin = sb.build();
+		// 明細
+		ArrayList<UriageMeisai> meisai = new ArrayList<UriageMeisai>();
+		// 明細01
+		UriageMeisaiBuilder umb = new UriageMeisaiBuilder();
+		umb.withHanbaiNumber(11);
+		umb.withShohinDomain(shohin);
+		UriageMeisai uriageMeisai = umb.build();
+		meisai.add(uriageMeisai);
+		// 部署
+		BushoBuilder bb = new BushoBuilder();
+		bb.withCode("BS01");
+		EigyoDate eigyoDate = new EigyoDate(2017, 8, 20);
+		bb.withEigyoDate(eigyoDate);
+		Busho busho = bb.build();
+		// 顧客
+		KokyakuBuilder kb = new KokyakuBuilder();
+		kb.withCode("KK01");
+		kb.withShukanBusho(busho);
+		Kokyaku kokyaku = kb.build();
+		// 売上
+		UriageBuilder ub = new UriageBuilder();
+		ub.withKokyaku(kokyaku);
+		ub.withDenpyoNumber("00001");
+		ub.withUriageMeisai(meisai);
+		Uriage uriage = ub.build();
 		// expect
 		new Expectations() {
 			{
-				uriageCrudService.exsists((TUriagePK) any);
-				result = false;
 				zaiko.getNumber();
-				result = 5;
+				result = 10;
 			}
 		};
 		// do
-		shohinIdoSpecificationImpl.setUriage(uriage02);
+		shohinIdoSpecificationImpl.setUriage(uriage);
 		shohinIdoSpecificationImpl.ascertainSatisfaction();
+	}
+
+	/**
+	 * 仕様確認.
+	 *
+	 * <pre>
+	 * 入力：売上<br>
+	 * 条件：在庫10個に対し、商品売上11個 。ただし訂正分で1個在庫がもどる。<br>
+	 * 結果：エラーなし
+	 * 
+	 * <pre>
+	 */
+	@Test
+	public void test05_ascertainSatisfaction(@Injectable ShohinZaiko zaiko, @Injectable ShohinIdo pastShohinIdo)
+			throws Exception {
+		// input
+		// 商品
+		ShohinBuilder sb = new ShohinBuilder();
+		sb.withCode("SH01");
+		Shohin shohin = sb.build();
+		// 明細
+		ArrayList<UriageMeisai> meisai = new ArrayList<UriageMeisai>();
+		// 明細01
+		UriageMeisaiBuilder umb = new UriageMeisaiBuilder();
+		umb.withHanbaiNumber(11);
+		umb.withShohinDomain(shohin);
+		UriageMeisai uriageMeisai = umb.build();
+		meisai.add(uriageMeisai);
+		// 部署
+		BushoBuilder bb = new BushoBuilder();
+		bb.withCode("BS01");
+		EigyoDate eigyoDate = new EigyoDate(2017, 8, 20);
+		bb.withEigyoDate(eigyoDate);
+		Busho busho = bb.build();
+		// 顧客
+		KokyakuBuilder kb = new KokyakuBuilder();
+		kb.withCode("KK01");
+		kb.withShukanBusho(busho);
+		Kokyaku kokyaku = kb.build();
+		// 売上
+		UriageBuilder ub = new UriageBuilder();
+		ub.withKokyaku(kokyaku);
+		ub.withDenpyoNumber("00001");
+		ub.withUriageMeisai(meisai);
+		ub.withRecordId("r-KK01-00001");
+		Uriage uriage = ub.build();
+		// 過去の商品移動モック
+		HashSet<Shohin> shohinSet = new HashSet<Shohin>();
+		shohinSet.add(shohin);
+		// expect
+		new Expectations() {
+			{
+				// 訂正対象の過去商品移動
+				shohinIdoUriageCrudService.getNewestShohinIdo(uriage.getRecordId());
+				result = Optional.of(pastShohinIdo);
+				// 当日営業日に移動した
+				pastShohinIdo.getDate();
+				result = new TheDate(2017, 8, 20);
+				// 対象商品商品。1つ移動した（部署->顧客）
+				pastShohinIdo.getShohinSet();
+				result = shohinSet;
+				pastShohinIdo.getAbusoluteIdoNumberForBushoZaiko(shohin);
+				result = -1;
+				// 在庫は10個残っている
+				zaiko.getNumber();
+				result = 10;
+			}
+		};
+		// do
+		shohinIdoSpecificationImpl.setUriage(uriage);
+		shohinIdoSpecificationImpl.ascertainSatisfaction();
+		// verify
+		new Verifications() {
+			{
+				shohinIdoUriageCrudService.getNewestShohinIdo(uriage.getRecordId());
+				times = 1;
+			}
+		};
 	}
 }
