156be80468b7af8e5b1277eeda2b8ad372ecbc14
test@example.com <Administrator@ZK0CA0C9.uodomain.local>
2018-03-15 08:58:08 +0900

    test BushoDateCrudService#getNext

--- src/main/java/com/showka/service/crud/u17/BushoDateCrudServiceImpl.java
+++ src/main/java/com/showka/service/crud/u17/BushoDateCrudServiceImpl.java
@@ -15,6 +15,7 @@ public class BushoDateCrudServiceImpl implements BushoDateCrudService {
 	@Autowired
 	private MBushoDateRepository repo;
 
+	// public methods
 	@Override
 	public void toNextEigyoDate(Busho busho) {
 		MBushoDate dateEntity = repo.getOne(busho.getRecordId());
@@ -25,9 +26,17 @@ public class BushoDateCrudServiceImpl implements BushoDateCrudService {
 
 	@Override
 	public EigyoDate getNext(Busho busho, EigyoDate eigyoDate) {
-		return getNext(eigyoDate);
+		return this.getNext(eigyoDate);
 	}
 
+	// private methods
+	/**
+	 * 次営業日取得.
+	 * 
+	 * @param date
+	 *            営業日
+	 * @return その次の営業日
+	 */
 	private EigyoDate getNext(EigyoDate date) {
 		return new EigyoDate(date.plusDays(1));
 	}
