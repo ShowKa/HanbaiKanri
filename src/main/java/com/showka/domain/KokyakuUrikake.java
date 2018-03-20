87a314d5f4582aa6e52a44104ddca361d64df5c7
test@example.com <Administrator@ZK0CA0C9.uodomain.local>
2018-03-20 14:05:10 +0900

    implement Urikake & KokyakuUrikake

---
+++ src/main/java/com/showka/domain/KokyakuUrikake.java
@@ -0,0 +1,98 @@
+package com.showka.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.showka.system.exception.SystemException;
+import com.showka.value.EigyoDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class KokyakuUrikake extends DomainBase {
+
+	// members
+	/** 顧客 */
+	private Kokyaku kokyaku;
+
+	/** 売掛(残高あり) */
+	private List<Urikake> urikakeList;
+
+	// public methods
+	/**
+	 * 売上の売掛金残高の合計金額を取得.
+	 * 
+	 * @return 合計
+	 */
+	public Integer getGokeiKingaku() {
+		return this.getGokeiKingaku(urikakeList);
+	}
+
+	/**
+	 * 指定した営業日までに入金予定日を迎えている売掛を取得する.
+	 * 
+	 * <pre>
+	 * 指定した営業日=入金予定日の場合も含まれる。
+	 * </pre>
+	 * 
+	 * @param date
+	 *            営業日
+	 * @return 入金が必要な売掛
+	 */
+	public List<Urikake> getUrikakeListNyukinRequiredBy(EigyoDate date) {
+		return urikakeList.parallelStream().filter(urikake -> {
+			// 入金予定日<=指定日付のものを抽出
+			EigyoDate nyukinYoteiDate = urikake.getNyukinYoteiDate();
+			return nyukinYoteiDate.isBefore(date) || nyukinYoteiDate.equals(date);
+		}).collect(Collectors.toList());
+	}
+
+	/**
+	 * 指定した営業日までに入金予定日を迎えている売掛の残高の合計金額を取得する.
+	 * 
+	 * <pre>
+	 * 指定した営業日=入金予定日の場合も含まれる。
+	 * </pre>
+	 * 
+	 * @param date
+	 *            営業日
+	 * @return 入金が必要な売掛の残高合計金額
+	 */
+	public Integer getGokeiKingakuNyukinRequiredBy(EigyoDate date) {
+		List<Urikake> _urikakeList = this.getUrikakeListNyukinRequiredBy(date);
+		return this.getGokeiKingaku(_urikakeList);
+	}
+
+	// override methods
+	@Override
+	public void validate() throws SystemException {
+		// do nothing
+	}
+
+	@Override
+	protected boolean equals(DomainBase other) {
+		KokyakuUrikake o = (KokyakuUrikake) other;
+		return kokyaku.equals(o.kokyaku);
+	}
+
+	@Override
+	public int hashCode() {
+		return kokyaku.hashCode();
+	}
+
+	// private methods
+	/**
+	 * 売掛の残高の合計金額を取得.
+	 * 
+	 * @param urikakeList
+	 *            売掛のリスト
+	 * @return 残高合計
+	 */
+	private Integer getGokeiKingaku(List<Urikake> urikakeList) {
+		return urikakeList.parallelStream().mapToInt(urikake -> {
+			return urikake.getZandaka();
+		}).sum();
+	}
+}
---
+++ src/main/java/com/showka/domain/builder/UrikakeBuilder.java
@@ -0,0 +1,139 @@
+package com.showka.domain.builder;
+
+import com.showka.domain.Uriage;
+import com.showka.domain.Urikake;
+import com.showka.value.EigyoDate;
+
+public class UrikakeBuilder extends com.showka.domain.builder.DomainBuilderBase<Urikake, UrikakeBuilder> {
+
+	// private member
+	/** uriage */
+	private Uriage uriage;
+
+	/** zandaka */
+	private Integer zandaka;
+
+	/** nyukinYoteiDate */
+	private EigyoDate nyukinYoteiDate;
+
+	/** recordId */
+	private String recordId;
+
+	/** version */
+	private Integer version;
+
+	// protected method
+	@Override
+	protected void apply(Urikake domain, UrikakeBuilder builder) {
+		builder.withUriage(domain.getUriage());
+		builder.withZandaka(domain.getZandaka());
+		builder.withNyukinYoteiDate(domain.getNyukinYoteiDate());
+		builder.withRecordId(domain.getRecordId());
+		builder.withVersion(domain.getVersion());
+	}
+
+	@Override
+	protected Urikake createDomainObject() {
+		Urikake domain = new Urikake(uriage, zandaka, nyukinYoteiDate);
+		domain.setRecordId(recordId);
+		domain.setVersion(version);
+		return domain;
+	}
+
+	@Override
+	protected UrikakeBuilder getThis() {
+		return this;
+	}
+
+	@Override
+	protected UrikakeBuilder newInstance() {
+		return new UrikakeBuilder();
+	}
+
+	// public method
+	/**
+	 * {@link Urikake}に与えるuriageをこのビルダに設定する。
+	 *
+	 * @param uriage
+	 *            uriage
+	 * @return {@link UrikakeBuilder}
+	 */
+	public UrikakeBuilder withUriage(final Uriage uriage) {
+		addConfigurator(new BuilderConfigurator<UrikakeBuilder>() {
+			@Override
+			public void configure(UrikakeBuilder builder) {
+				builder.uriage = uriage;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link Urikake}に与えるzandakaをこのビルダに設定する。
+	 *
+	 * @param zandaka
+	 *            zandaka
+	 * @return {@link UrikakeBuilder}
+	 */
+	public UrikakeBuilder withZandaka(final Integer zandaka) {
+		addConfigurator(new BuilderConfigurator<UrikakeBuilder>() {
+			@Override
+			public void configure(UrikakeBuilder builder) {
+				builder.zandaka = zandaka;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link Urikake}に与えるnyukinYoteiDateをこのビルダに設定する。
+	 *
+	 * @param nyukinYoteiDate
+	 *            nyukinYoteiDate
+	 * @return {@link UrikakeBuilder}
+	 */
+	public UrikakeBuilder withNyukinYoteiDate(final EigyoDate nyukinYoteiDate) {
+		addConfigurator(new BuilderConfigurator<UrikakeBuilder>() {
+			@Override
+			public void configure(UrikakeBuilder builder) {
+				builder.nyukinYoteiDate = nyukinYoteiDate;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link Urikake}に与えるrecordIdをこのビルダに設定する。
+	 *
+	 * @param recordId
+	 *            recordId
+	 * @return {@link UrikakeBuilder}
+	 */
+	public UrikakeBuilder withRecordId(final String recordId) {
+		addConfigurator(new BuilderConfigurator<UrikakeBuilder>() {
+			@Override
+			public void configure(UrikakeBuilder builder) {
+				builder.recordId = recordId;
+			}
+		});
+		return getThis();
+	}
+
+	/**
+	 * {@link Urikake}に与えるversionをこのビルダに設定する。
+	 *
+	 * @param version
+	 *            version
+	 * @return {@link UrikakeBuilder}
+	 */
+	public UrikakeBuilder withVersion(final Integer version) {
+		addConfigurator(new BuilderConfigurator<UrikakeBuilder>() {
+			@Override
+			public void configure(UrikakeBuilder builder) {
+				builder.version = version;
+			}
+		});
+		return getThis();
+	}
+
+}
