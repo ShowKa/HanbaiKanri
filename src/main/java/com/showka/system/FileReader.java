178245d9e95c5ace883c72dd86c00135abba17f4
John Doe <johndoe@example.com>
2018-05-20 23:04:44 +0900

    load csv

---
+++ src/main/java/com/showka/system/FileReader.java
@@ -0,0 +1,49 @@
+package com.showka.system;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.showka.system.exception.SystemException;
+
+public class FileReader {
+
+	/**
+	 * CSVを指定した型にパース.
+	 * 
+	 * @param type
+	 *            型
+	 * @param filePath
+	 *            ファイルパス
+	 * @return 型で指定したオブジェクトのリスト
+	 * @throws IOException
+	 */
+	public static <T> List<T> read(Class<T> type, String filePath) {
+		File file = new File(filePath);
+		return read(type, file);
+	}
+
+	/**
+	 * CSVを指定した型にパース.
+	 * 
+	 * @param type
+	 *            型
+	 * @param file
+	 *            CSVファイル
+	 * @return 型で指定したオブジェクトのリスト
+	 * @throws IOException
+	 */
+	public static <T> List<T> read(Class<T> type, File file) {
+		CsvMapper mapper = new CsvMapper();
+		CsvSchema schema = CsvSchema.emptySchema().withHeader();
+		try {
+			MappingIterator<T> readValues = mapper.readerFor(type).with(schema).readValues(file);
+			return readValues.readAll();
+		} catch (IOException e) {
+			throw new SystemException("ファイル読み込み失敗", e);
+		}
+	}
+}
---
+++ src/test/java/com/showka/system/FileReaderTest.java
@@ -0,0 +1,44 @@
+package com.showka.system;
+
+import java.io.File;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.showka.common.SimpleTestCase;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class FileReaderTest extends SimpleTestCase {
+
+	@Test
+	public void test01_read() throws Exception {
+		String path = "/data/system/FileReaderTest.csv";
+		File file = new ClassPathResource(path).getFile();
+		String absoluteFilePath = file.getAbsolutePath();
+		List<DTO> result = FileReader.read(DTO.class, absoluteFilePath);
+		assertEquals("ABC", result.get(1).getNamae());
+	}
+
+	@Test
+	public void test02_read() throws Exception {
+		String path = "/data/system/FileReaderTest.csv";
+		File file = new ClassPathResource(path).getFile();
+		List<DTO> result = FileReader.read(DTO.class, file);
+		assertEquals("ABC", result.get(1).getNamae());
+	}
+
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	static public class DTO {
+		@JsonProperty("name")
+		String namae;
+		@JsonProperty("id")
+		String id;
+	}
+}
