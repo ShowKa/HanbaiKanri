122d0b7210cf4bb6c30d78b7f7e8d1174378ed3b
test@example.com <Administrator@ZK0CA0C9.uodomain.local>
2018-05-21 10:35:41 +0900

    @PathVariable -> @RequestParam

--- src/main/java/com/showka/web/u08/U08B001Controller.java
+++ src/main/java/com/showka/web/u08/U08B001Controller.java
@@ -1,9 +1,11 @@
 package com.showka.web.u08;
 
+import java.util.HashMap;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.showka.web.ControllerBase;
@@ -12,8 +14,21 @@ import com.showka.web.ControllerBase;
 @RequestMapping("api/u08b001")
 public class U08B001Controller extends ControllerBase {
 
-	@RequestMapping(method = RequestMethod.GET, value = "{filePath}")
-	public ResponseEntity<?> loadNyukin(@PathVariable("filePath") String filePath) {
-		return ResponseEntity.ok(new Object());
+	// how to request
+	// $user = "user01"
+	// $pass = "pass"
+	// $secp= ConvertTo-SecureString $PASS -AsPlainText -Force
+	// $cred = New-Object System.Management.Automation.PSCredential($user,$secp)
+	// $uri = "http://localhost:8080/api/u08b001?filePath=abc/def.txt"
+	// Invoke-RestMethod -Uri $uri -Credential $cred
+
+	// 通常「@PathVariable」を使う
+	// しかしパラメータにスラッシュ（FileSeparator）を含むとうまく動かないので、@RequestParamにしてる。
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<?> loadNyukin(@RequestParam("filePath") String filePath) {
+		HashMap<String, String> m = new HashMap<String, String>();
+		m.put("param", filePath);
+		m.put("result", "success!");
+		return ResponseEntity.ok(m);
 	}
 }
