4513e45f2e089646d9ddff878794cba1fa1510ee
John Doe <johndoe@example.com>
2018-08-16 22:54:19 +0900

    FirmBankFurikomiMatchingErrorCrudService

---
+++ src/main/java/com/showka/repository/i/WFirmBankFurikomiMatchingErrorRepository.java
@@ -0,0 +1,10 @@
+package com.showka.repository.i;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.showka.entity.WFirmBankFurikomiMatchingError;
+
+public interface WFirmBankFurikomiMatchingErrorRepository
+		extends JpaRepository<WFirmBankFurikomiMatchingError, String> {
+
+}
