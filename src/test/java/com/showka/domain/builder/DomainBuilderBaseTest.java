e6004f483d1448692a7927cabe8a4f4d48e8f4a2
John Doe <johndoe@example.com>
2018-09-22 20:39:26 +0900

    DomainBuilder修正:DomainAggregationにも対応

--- src/test/java/com/showka/domain/builder/DomainBuilderBaseTest.java
+++ src/test/java/com/showka/domain/builder/DomainBuilderBaseTest.java
@@ -11,18 +11,22 @@ public class DomainBuilderBaseTest extends TestCase {
 
 	@Test
 	public void test01_Apply() throws Exception {
-
 		UriageBuilder ub1 = new UriageBuilder();
 		ub1.withDenpyoNumber("00001");
 		ub1.withKeijoDate(new TheDate(2000, 1, 1));
 		Uriage u1 = ub1.build();
-
 		UriageBuilder ub2 = new UriageBuilder();
 		ub2.withDenpyoNumber("00002");
 		Uriage u2 = ub2.apply(u1);
-
 		assertEquals("00002", u2.getDenpyoNumber());
 		assertEquals(new TheDate(2000, 1, 1), u2.getKeijoDate());
 	}
 
+	// record is null -> empty string
+	@Test
+	public void test02_Apply_id() throws Exception {
+		UriageBuilder ub = new UriageBuilder();
+		Uriage actual = ub.build();
+		assertEquals("", actual.getRecordId());
+	}
 }
