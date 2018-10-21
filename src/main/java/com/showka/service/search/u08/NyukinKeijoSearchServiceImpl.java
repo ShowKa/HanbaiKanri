2f4bbf66b6cf9e37ff81e8d7ee6214c064276258
John Doe <johndoe@example.com>
2018-10-21 22:20:17 +0900

    NyukinKeijoSeachService impl

--- src/main/java/com/showka/service/search/u08/NyukinKeijoSearchServiceImpl.java
+++ src/main/java/com/showka/service/search/u08/NyukinKeijoSearchServiceImpl.java
@@ -1,10 +1,19 @@
 package com.showka.service.search.u08;
 
+import static com.showka.table.public_.tables.T_NYUKIN.*;
+import static com.showka.table.public_.tables.T_NYUKIN_KEIJO.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SelectConditionStep;
+import org.jooq.SelectJoinStep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
 
 import com.showka.domain.u08.Nyukin;
 import com.showka.domain.z00.Busho;
@@ -12,9 +21,16 @@ import com.showka.entity.TNyukinKeijo;
 import com.showka.repository.i.TNyukinKeijoRepository;
 import com.showka.service.crud.u08.i.NyukinCrudService;
 import com.showka.service.search.u08.i.NyukinKeijoSearchService;
+import com.showka.table.public_.tables.T_NYUKIN;
+import com.showka.table.public_.tables.T_NYUKIN_KEIJO;
+import com.showka.table.public_.tables.records.T_NYUKIN_RECORD;
 import com.showka.value.EigyoDate;
 
+@Service
 public class NyukinKeijoSearchServiceImpl implements NyukinKeijoSearchService {
+
+	@Autowired
+	private DSLContext create;
 
 	@Autowired
 	private TNyukinKeijoRepository repo;
@@ -22,9 +38,33 @@ public class NyukinKeijoSearchServiceImpl implements NyukinKeijoSearchService {
 	@Autowired
 	private NyukinCrudService nyukinCrudService;
 
+	// alias
+	private static final T_NYUKIN nk = t_nyukin.as("nk");
+	private static final T_NYUKIN_KEIJO kj = t_nyukin_keijo.as("kj");
+
 	@Override
 	public List<Nyukin> searchNotDone(Busho busho, EigyoDate kijunDate) {
-		return null;
+		Result<T_NYUKIN_RECORD> results = this._searchNotDone(busho, kijunDate);
+		return results.stream().map(r -> {
+			return nyukinCrudService.getDomain(r.getRecordId());
+		}).collect(Collectors.toList());
+	}
+
+	Result<T_NYUKIN_RECORD> _searchNotDone(Busho busho, EigyoDate kijunDate) {
+		// select * from 入金
+		SelectJoinStep<Record> from = create.select().from(nk);
+		// inner join 入金計上
+		from.leftJoin(kj).on(nk.record_id.eq(kj.nyukin_id));
+		// where
+		SelectConditionStep<Record> where = from.where();
+		// 入金.部署 = 引数.部署
+		where.and(nk.busho_id.eq(busho.getRecordId()));
+		// 入金日 <= 引数.基準日
+		where.and(nk.date.lessOrEqual(kijunDate.toLocalDateTime()));
+		// 未計上
+		where.and(kj.nyukin_id.isNull());
+		// fetch
+		return where.fetchInto(nk);
 	}
 
 	@Override
