9dff642bf544e70987b0128f9727950463fa5dbd
John Doe <johndoe@example.com>
2018-11-01 21:43:21 +0900

    Value: ShimeDate 作成

---
+++ src/main/java/com/showka/value/ShimeDate.java
@@ -0,0 +1,54 @@
+package com.showka.value;
+
+import com.showka.system.exception.SystemException;
+
+import lombok.Getter;
+
+@Getter
+public class ShimeDate extends ValueBase implements Comparable<ShimeDate> {
+
+	/** 締日 */
+	private Integer dateValue;
+
+	/**
+	 * 締日生成.
+	 * 
+	 * @param date
+	 *            締日(1-30を想定)
+	 */
+	public ShimeDate(Integer date) {
+		this.dateValue = date;
+		this.validate();
+	}
+
+	/**
+	 * 締日生成.
+	 * 
+	 * @param date
+	 *            締日(1-30を想定)
+	 */
+	public ShimeDate(TheDate date) {
+		this.dateValue = date.getDate().getDayOfMonth();
+		this.validate();
+	}
+
+	/**
+	 * 整合性検証.
+	 */
+	private void validate() {
+		if (this.dateValue < 0 || this.dateValue > 30) {
+			throw new SystemException("不正の締日が指定されました: " + dateValue);
+		}
+	}
+
+	@Override
+	protected boolean equals(ValueBase other) {
+		ShimeDate o = (ShimeDate) other;
+		return this.dateValue.equals(o.dateValue);
+	}
+
+	@Override
+	public int compareTo(ShimeDate o) {
+		return this.dateValue.compareTo(o.dateValue);
+	}
+}
