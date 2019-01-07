1c36fa0c31cc0d0ad18308fee3de642c399df3bc
John Doe <johndoe@example.com>
2019-01-07 21:45:40 +0900

    implement NyukaTeiseiCrud

--- src/main/java/com/showka/entity/TShohinIdoNyukaTeisei.java
+++ src/main/java/com/showka/entity/TShohinIdoNyukaTeisei.java
@@ -2,10 +2,9 @@ package com.showka.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -25,15 +24,9 @@ public class TShohinIdoNyukaTeisei extends EntityBase implements Serializable {
 	/** SID. */
 	private static final long serialVersionUID = -7136464915357710337L;
 
-	/** 商品移動ID */
-	@Id
-	@Column(name = "shohin_ido_id", nullable = false, length = 255)
-	private String shohinIdoId;
-
-	/** 入荷ID（訂正対象） */
-	@Id
-	@Column(name = "nyuka_id", nullable = false, length = 255)
-	private String nyukaId;
+	/** PK */
+	@EmbeddedId
+	private TShohinidoNyukaTeiseiPK pk;
 
 	// fetch
 	/** 商品移動. */
