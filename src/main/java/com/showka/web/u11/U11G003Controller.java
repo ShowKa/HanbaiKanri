d33bbcfc9bed79ce4564b3fefc1ba760bd0c6b0a
John Doe <johndoe@example.com>
2019-01-22 00:17:09 +0900

    画面構築中....

--- src/main/java/com/showka/web/u11/U11G003Controller.java
+++ src/main/java/com/showka/web/u11/U11G003Controller.java
@@ -15,7 +15,7 @@ import com.showka.web.ModelAndViewExtended;
 @EnableAutoConfiguration
 public class U11G003Controller extends ControllerBase {
 
-	/** 参照. */
+	/** 参照モード. */
 	@RequestMapping(value = "/u11g003/refer", method = RequestMethod.GET)
 	public ModelAndViewExtended refer(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
 		// set model
@@ -26,20 +26,76 @@ public class U11G003Controller extends ControllerBase {
 		return model;
 	}
 
-	/** 商品入荷登録. */
-	@RequestMapping(value = "/u11g003/register", method = RequestMethod.GET)
-	public ModelAndViewExtended register(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+	/** 新規登録モード */
+	@RequestMapping(value = "/u11g003/registerForm", method = RequestMethod.GET)
+	public ModelAndViewExtended registerForm(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
 		// set model
 		model.addForm(form);
-		model.setMode(Mode.READ);
+		model.setMode(Mode.REGISTER);
 		model.setViewName("/u11/u11g003");
 		// return
 		return model;
 	}
 
-	// 取得
+	/** 商品入荷登録. */
+	@RequestMapping(value = "/u11g003/register", method = RequestMethod.POST)
+	public ResponseEntity<?> register(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 商品入荷更新. */
+	@RequestMapping(value = "/u11g003/update", method = RequestMethod.POST)
+	public ResponseEntity<?> update(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 商品入荷削除. */
+	@RequestMapping(value = "/u11g003/delete", method = RequestMethod.POST)
+	public ResponseEntity<?> delete(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 商品入荷訂正登録. */
+	@RequestMapping(value = "/u11g003/registerTeisei", method = RequestMethod.POST)
+	public ResponseEntity<?> registerTeisei(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 商品入荷訂正更新. */
+	@RequestMapping(value = "/u11g003/updateTeisei", method = RequestMethod.POST)
+	public ResponseEntity<?> updateTeisei(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 商品入荷訂正削除. */
+	@RequestMapping(value = "/u11g003/deleteTeisei", method = RequestMethod.POST)
+	public ResponseEntity<?> deleteTeisei(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 商品入荷取得. */
 	@RequestMapping(value = "/u11g003/get", method = RequestMethod.POST)
 	public ResponseEntity<?> get(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** ヘッダー整合性検証. */
+	@RequestMapping(value = "/u11g003/validateHeader", method = RequestMethod.POST)
+	public ResponseEntity<?> validateHeader(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
+		// return
+		return ResponseEntity.ok(model);
+	}
+
+	/** 明細整合性検証. */
+	@RequestMapping(value = "/u11g003/validateMeisai", method = RequestMethod.POST)
+	public ResponseEntity<?> validateMeisai(@ModelAttribute U11G003Form form, ModelAndViewExtended model) {
 		// return
 		return ResponseEntity.ok(model);
 	}
---
+++ src/main/resources/static/u11/u11g003.js
@@ -0,0 +1,181 @@
+ngModules.service('nyuka', [ '$rootScope', '$filter', '$httpw',
+// モデルの操作・取得
+function($scope, $filter, $httpw) {
+	// service
+} ])
+// controllers
+.controller('MainController', [ '$scope', '$httpw', 'nyuka', 'common', 'meisai',
+// main
+function($scope, $httpw, nyukaService, common, meisaiService) {
+	// scope
+	/**
+	 * 初期化.
+	 */
+	$scope.initialize = function() {
+		$scope.header = {
+			editing : true,
+			edit : function(editable) {
+				this.editing = editable;
+			},
+		};
+		if (!$scope.meisaiList) {
+			$scope.meisaiList = [];
+		}
+	};
+	/**
+	 * 参照モード
+	 */
+	$scope.toRead = function() {
+		common.toRead();
+	};
+	/**
+	 * 更新モード
+	 */
+	$scope.toUpdate = function() {
+		common.toUpdate();
+	};
+	/**
+	 * 訂正モード
+	 */
+	$scope.toRegisterTeiseai = function() {
+	};
+	/**
+	 * 訂正更新モード
+	 */
+	$scope.toUpdateTeisei = function() {
+	};
+	/**
+	 * 商品入荷取得.
+	 */
+	$scope.get = function(nyukaId) {
+		var callback = function() {
+		};
+		$httpw.post("/u11g003/get", {
+			nyukaId : nyukaId,
+		}, callback);
+	};
+	/**
+	 * 明細リスト入力開始.
+	 */
+	$scope.startEditingMeisaiList = function() {
+		// validateHeader
+		var callback = function() {
+			$scope.header.edit(false);
+			$scope.addMeisai();
+		};
+		$httpw.post("/u11g003/validateHeader", {}, callback);
+	};
+	/**
+	 * 明細追加
+	 */
+	$scope.addMeisai = function() {
+		$scope.meisaiList.push(createMeisai());
+	};
+	/**
+	 * 明細編集完了.
+	 */
+	$scope.done = function(meisai) {
+		var callback = function() {
+			meisai.editDone();
+		}
+		$httpw.post("/u11g003/validateMeisai", {
+			// ヘッダ
+			bushoCode : $scope.bushoCode,
+			// 明細
+			"meisai[0].shohinCode" : meisai.shohinCode,
+		}, callback);
+	};
+	/**
+	 * 明細編集.
+	 */
+	$scope.edit = function(meisai) {
+		meisai.edit();
+	}
+	/**
+	 * 明細削除.
+	 */
+	$scope.removeMeisai = function(target) {
+		meisaiService.remove(target, $scope.meisaiList);
+	};
+	/**
+	 * 新規登録.
+	 */
+	$scope.register = function() {
+		// check
+		if (!meisaiService.check($scope.meisaiList)) {
+			return;
+		}
+		var callback = function() {
+			// 参照モードへ
+			common.toRead();
+		};
+		$httpw.post("/u11g003/register", {}, callback);
+	};
+	/**
+	 * 更新.
+	 */
+	$scope.update = function() {
+		var callback = function () {
+		};
+		$httpw.post("/u11g003/update", {}, callback);
+	};
+	/**
+	 * 削除.
+	 */
+	$scope.delete = function() {
+		var callback = function () {
+		};
+		$httpw.post("/u11g003/delete", {}, callback);
+	};
+	/**
+	 * 訂正登録.
+	 */
+	$scope.registerTeisei = function() {
+		if (!meisaiService.check($scope.meisaiList)) {
+			return;
+		}
+		var callback = function() {
+			common.toRead();
+		};
+		$httpw.post("/u11g003/registerTeisei", {}, callback);
+	};
+	/**
+	 * 訂正更新.
+	 */
+	$scope.updateTeisei = function() {
+		var callback = function () {
+			common.toRead();
+		};
+		$httpw.post("/u11g003/updateTeisei", {}, callback);
+	};
+	/**
+	 * 訂正削除.
+	 */
+	$scope.deleteTeisei = function() {
+		var callback = function () {
+			common.toRead();
+		};
+		$httpw.post("/u11g003/deleteTeisei", {}, callback);
+	}
+
+	// 内部関数
+	/**
+	 * 明細作成
+	 */
+	var createMeisai = function() {
+		var m = {
+			shohinCode : '',
+		};
+		meisaiService.convertToMeisai(m);
+		m.edit();
+		return m;
+	};
+	// 初期処理
+	if ($scope.isReadMode()) {
+		var nyukaId = "入荷IDを設定してください。";
+		$scope.get(nyukaId);
+	} else if ($scope.isRegisterMode()) {
+		// 初期化
+		$scope.initialize();
+	}
+} ]);
