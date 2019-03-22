3c453771d2f6332f336804d6678d196686107c02
John Doe <johndoe@example.com>
2019-03-22 17:24:51 +0900

    bug fix

--- src/main/java/com/showka/service/validator/z00/ShohinValidatorImpl.java
+++ src/main/java/com/showka/service/validator/z00/ShohinValidatorImpl.java
@@ -1,12 +1,17 @@
 package com.showka.service.validator.z00;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.showka.service.crud.z00.i.ShohinCrud;
 import com.showka.service.validator.z00.i.ShohinValidator;
 import com.showka.system.exception.validate.NotExistException;
 
+@Service
 public class ShohinValidatorImpl implements ShohinValidator {
 
 	/** 商品CRUD */
+	@Autowired
 	private ShohinCrud shohinCrud;
 
 	@Override
