51d81d83dbb46eeb041dc21a0bbf00261f8bdcf9
John Doe <johndoe@example.com>
2019-04-11 12:22:30 +0900

    訂正検証#当日営業日に入荷訂正登録済

--- src/main/java/com/showka/service/validator/u11/NyukaTeiseiValidatorImpl.java
+++ src/main/java/com/showka/service/validator/u11/NyukaTeiseiValidatorImpl.java
@@ -1,6 +1,7 @@
 package com.showka.service.validator.u11;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,18 +33,20 @@ public class NyukaTeiseiValidatorImpl implements NyukaTeiseiValidator {
 	@Override
 	public void validateForTeisei(Nyuka nyuka, String teiseiShohinIdoId) throws CanNotUpdateOrDeleteException {
 		// 入荷日検証
-		this.validateNyukaDate(nyuka);
+		this.errorIfNyukaDateIsEigyoDate(nyuka);
 		// 棚卸検証
-		this.validateTanaoroshi(nyuka);
+		this.errorIfTanaoroshiDone(nyuka);
 		// 在庫検証
 		ShohinIdo s = nyuka.getShohinIdoById(teiseiShohinIdoId);
 		shohinZaikoValidator.validateMinusZaiko(s);
+		// 当日訂正済検証
+		this.errorIfTeiseiAlreadyDoneInEigyoDate(nyuka);
 	}
 
 	@Override
 	public void validateForTeiseiUpdate(Nyuka nyuka, String teiseiShohinIdoId) throws CanNotUpdateOrDeleteException {
 		// 訂正済検証
-		this.validateAlreadyDone(nyuka, teiseiShohinIdoId);
+		this.errorIfTeiseiNoteYetInEigyoDate(nyuka, teiseiShohinIdoId);
 		// 在庫検証
 		ShohinIdo s = nyuka.getShohinIdoById(teiseiShohinIdoId);
 		shohinZaikoValidator.validateMinusZaiko(s);
@@ -52,13 +55,13 @@ public class NyukaTeiseiValidatorImpl implements NyukaTeiseiValidator {
 	@Override
 	public void validateForTeiseiDelete(Nyuka nyuka, String teiseiShohinIdoId) throws CanNotUpdateOrDeleteException {
 		// 訂正済検証
-		this.validateAlreadyDone(nyuka, teiseiShohinIdoId);
+		this.errorIfTeiseiNoteYetInEigyoDate(nyuka, teiseiShohinIdoId);
 	}
 
 	/**
-	 * 入荷日検証.
+	 * 入荷日=営業日の場合エラー
 	 */
-	void validateNyukaDate(Nyuka nyuka) {
+	void errorIfNyukaDateIsEigyoDate(Nyuka nyuka) {
 		EigyoDate nyukaDate = nyuka.getNyukaDate();
 		EigyoDate eigyoDate = nyuka.getBusho().getEigyoDate();
 		if (nyukaDate.equals(eigyoDate)) {
@@ -67,9 +70,9 @@ public class NyukaTeiseiValidatorImpl implements NyukaTeiseiValidator {
 	}
 
 	/**
-	 * 棚卸検証.
+	 * 棚卸済の場合エラー
 	 */
-	void validateTanaoroshi(Nyuka nyuka) {
+	void errorIfTanaoroshiDone(Nyuka nyuka) {
 		Busho busho = nyuka.getBusho();
 		TheTimestamp start = nyuka.getNyukaShohinIdo().getTimestamp();
 		boolean done = shohinTanaoroshiQuery.doneBetween(busho, start);
@@ -79,9 +82,23 @@ public class NyukaTeiseiValidatorImpl implements NyukaTeiseiValidator {
 	}
 
 	/**
-	 * 訂正済検証.
+	 * 当日営業日訂正済みの場合エラー
 	 */
-	void validateAlreadyDone(Nyuka nyuka, String teiseiShohinIdoId) {
+	void errorIfTeiseiAlreadyDoneInEigyoDate(Nyuka nyuka) {
+		EigyoDate eigyoDate = nyuka.getBusho().getEigyoDate();
+		List<ShohinIdo> teiseiList = nyukaTeiseiCrud.getAll(nyuka.getRecordId());
+		Optional<ShohinIdo> same = teiseiList.stream().filter(t -> {
+			return t.getDate().equals(eigyoDate);
+		}).findAny();
+		if (same.isPresent()) {
+			throw new CanNotUpdateOrDeleteException("当日に入荷訂正を行っているため本伝票は入荷訂正の処理の対象外です");
+		}
+	}
+
+	/**
+	 * 当日営業日に未訂正の場合エラー
+	 */
+	void errorIfTeiseiNoteYetInEigyoDate(Nyuka nyuka, String teiseiShohinIdoId) {
 		List<ShohinIdo> teiseiShohinIdoList = nyukaTeiseiCrud.getAll(nyuka.getRecordId());
 		boolean done = teiseiShohinIdoList.stream().map(s -> s.getRecordId()).collect(Collectors.toList()).contains(
 				teiseiShohinIdoId);
